9d20c8534a16d18e51ef5906248fa865a8fdb0f0
summu89 <ardennbella@gmail.com>
2026-04-14 19:49:02 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -9,7 +9,7 @@ from Script import script
 from pyrogram.errors import ChatAdminRequired
 import asyncio 
 
-"""----------------------------------------- https://t.me/+g1YV0iHUSQFlYjM9 --------------------------------------"""
+"""----------------------------------------- https://t.me/ --------------------------------------"""
 
 @Client.on_message(filters.new_chat_members & filters.group)
 async def save_group(bot, message):
@@ -23,7 +23,7 @@ async def save_group(bot, message):
         if message.chat.id in temp.BANNED_CHATS:
             # Inspired from a boat of a banana tree
             buttons = [[
-                InlineKeyboardButton('Support', url='https://t.me/+g1YV0iHUSQFlYjM9')
+                InlineKeyboardButton('Support', url='https://t.me/')
             ]]
             reply_markup=InlineKeyboardMarkup(buttons)
             k = await message.reply(
@@ -38,8 +38,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-                    InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ', url='https://t.me/+g1YV0iHUSQFlYjM9'),
-                    InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇꜱ', url='https://t.me/+g1YV0iHUSQFlYjM9')
+                    InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ', url='https://t.me/'),
+                    InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇꜱ', url='https://t.me/')
                  ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
@@ -80,7 +80,7 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('Support', url='https://t.me/+g1YV0iHUSQFlYjM9')
+            InlineKeyboardButton('Support', url='https://t.me/')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
@@ -119,7 +119,7 @@ async def disable_chat(bot, message):
     await message.reply('Chat Successfully Disabled')
     try:
         buttons = [[
-            InlineKeyboardButton('Support', url='https://telegram.me/weebs_support')
+            InlineKeyboardButton('Support', url='https://telegram.me/')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
